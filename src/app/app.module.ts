cbb8873bd96d3368607292d6ce8ed3439db69273
Ronaldo <99ronaldoo99@gmail.com>
2018-11-26 17:31:00 -0500

    se importaron las dependencias del acelerometro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { FormularioPage } from '../pages/formulario/formulario';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { DeviceMotion, DeviceMotionAccelerationData } from '@ionic-native/device-motion'
 
 @NgModule({
   declarations: [
@@ -37,6 +38,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    DeviceMotion,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DeviceMotion, DeviceMotionAccelerationData } from '@ionic-native/device-motion'
 
 @Component({
   selector: 'page-home',
@@ -7,14 +8,34 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	x:string;
+	y:string;
+	z:string;
+	timeStamp:string;
+	id:any;
+  constructor(
+  	public navCtrl: NavController,
+  	public deviceMotion : DeviceMotion) {
+  	this.x ="-";
+  	this.y ="-";
+  	this.z ="-";
+  	this.timeStamp ="-"; 
 
   }
   start(){
+  	try{
+  		// var option: DeviceMotionAccelerationData=
+  		// {
+  		// 	frequency:200
+  		// };
 
+  	}catch(err)
+  	{
+  		alert("Error" + err);
+  	}
   }
   stop(){
-  	
+
   }
 
 }
